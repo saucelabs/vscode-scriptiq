812fba21466bf867b19a3f808312b338f53963fb
Alex Plischke <alex.plischke@saucelabs.com>
2024-02-29 13:28:19 -0800

    chore: remove obsolete code

--- src/utilities/utilities-service.ts
+++ src/utilities/utilities-service.ts
@@ -2,65 +2,6 @@ import { Uri, Webview } from 'vscode';
 import * as vscode from 'vscode';
 
 /**
- * Set storeData into context.globalState.
- * @param context :vscode.ExtensionContext
- * @param storeData : any
- */
-export function setStoreData(
-  context: vscode.ExtensionContext,
-  storeData: any,
-  key: string,
-) {
-  const state = stateManager(context);
-
-  if (storeData !== undefined) {
-    state.write(
-      {
-        storeData: storeData,
-      },
-      key,
-    );
-  }
-}
-
-/**
- * Gets storeData from context.globalState.
- * @param context :vscode.ExtensionContext
- * @returns string
- */
-export function getStoreData(
-  context: vscode.ExtensionContext,
-  key: string,
-): any {
-  const state = stateManager(context);
-
-  const { storeData } = state.read(key);
-  return storeData as any;
-}
-
-/**
- * State Manager has read and write methods for api key. This methods set and get the api key from context.globalState.
- * @param context :vscode.ExtensionContext.
- * @returns void.
- */
-export function stateManager(context: vscode.ExtensionContext) {
-  return {
-    read,
-    write,
-  };
-
-  function read(key: string) {
-    return {
-      storeData: context.globalState.get(key),
-    };
-  }
-
-  function write(newState: any, key: string) {
-    context.globalState.update(key, newState.storeData);
-  }
-}
-
-/**
  * Create a vscode.Uri as WebviewUri for source files.
  * @param webview :vscode.Weview
  * @param extensionUri :vscode.Uri

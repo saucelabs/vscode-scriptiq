646973a6e3cc2ec575d985bb1d20c1935677f537
Alex Plischke <alex.plischke@saucelabs.com>
2024-03-06 16:38:41 -0800

    fix: add missing conversions

--- media/test-generation.js
+++ media/test-generation.js
@@ -230,7 +230,7 @@ function generateFullTestDisplay() {
   // if (!languageChange) {
   vscode.postMessage({
     action: 'copy-image',
-    testID: data.test_id,
+    test_id: data.test_id,
   });
   timeoutTime = 400;
   // }
@@ -793,7 +793,7 @@ function sendUserRating(rating, step, testID) {
   console.log(`Sending User Rating for step ${step} of ${testID}: ${rating}`);
   vscode.postMessage({
     action: 'send-user-rating',
-    data: { rating: rating, step: step, testID: testID },
+    data: { rating: rating, step: step, test_id: testID },
   });
 }
 

51cddb67fa1e26d7c4008639e9dbe5ed7b78fb0c
Alex Plischke <alex.plischke@saucelabs.com>
2024-05-20 15:54:51 -0700

    build: correctly assemble distributable with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,41 +8,55 @@ const path = require('path');
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
 /** @type WebpackConfig */
-const extensionConfig = {
-  target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+const baseConfig = {
   mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-
-  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
-  output: {
-    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2',
-  },
   externals: {
     vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
     extensions: ['.ts', '.js'],
+  },
+  devtool: 'nosources-source-map',
+  infrastructureLogging: {
+    level: 'log', // enables logging required for problem matchers
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader',
-          },
-        ],
+        use: [{ loader: 'ts-loader' }],
       },
     ],
   },
-  devtool: 'nosources-source-map',
-  infrastructureLogging: {
-    level: 'log', // enables logging required for problem matchers
+};
+
+// Config for extension source code (to be run in a Node-based context)
+/** @type WebpackConfig */
+const extensionConfig = {
+  ...baseConfig,
+  target: 'node',
+  entry: './src/extension.ts',
+  externals: ['vscode'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
   },
 };
-module.exports = [extensionConfig];
+
+// Config for webview source code (to be run in a web-based context)
+/** @type WebpackConfig */
+const webviewConfig = {
+  ...baseConfig,
+  target: ['web', 'es2020'],
+  entry: './webview-ui/connect/build/assets/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webview-connect.js',
+    libraryTarget: 'commonjs2',
+  },
+};
+
+module.exports = [extensionConfig, webviewConfig];

8bb370ef4157b8ee242ff73341cbfed121d41db3
Alex Plischke <alex.plischke@saucelabs.com>
2024-06-17 14:39:27 -0700

    refactor: better exposure of cause of error

--- src/error.ts
+++ src/error.ts
@@ -1,9 +1,24 @@
 export function errMsg(obj: unknown): string {
-  if (isError(obj)) {
-    return obj.message;
-  } else {
-    return JSON.stringify(obj);
+  if (obj === null || obj === undefined) {
+    return '';
   }
+
+  if (isError(obj)) {
+    let msg = obj.message;
+
+    if (obj.cause) {
+      msg += `: ${errMsg(obj.cause)}`;
+    }
+
+    if (obj instanceof AggregateError && obj.errors.length > 0) {
+      // Show only the first error, because retries can cause repeated errors.
+      msg += `: ${errMsg(obj.errors[0])}`;
+    }
+
+    return msg;
+  }
+
+  return JSON.stringify(obj);
 }
 
 export function isError(obj: unknown): obj is Error {

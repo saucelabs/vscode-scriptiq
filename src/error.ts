831952ce8d3571637776831119a89bdbef048d9e
Alex Plischke <alex.plischke@saucelabs.com>
2024-06-17 14:05:35 -0700

    refactor: reuse

--- src/error.ts
+++ src/error.ts
@@ -1,6 +1,6 @@
 export function errMsg(obj: unknown): string {
-  if (typeof obj === 'object' && obj !== null && 'message' in obj) {
-    return (obj as Error).message;
+  if (isError(obj)) {
+    return obj.message;
   } else {
     return JSON.stringify(obj);
   }

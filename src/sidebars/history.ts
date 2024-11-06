cc7c68f40feb6f36272a936d4641e0b32690c23c
Mike Han <mike.han@saucelabs.com>
2024-11-06 11:54:22 -0700

    Don't render children of TestRecord

--- src/sidebars/history.ts
+++ src/sidebars/history.ts
@@ -1,20 +1,13 @@
 import * as vscode from 'vscode';
-import * as path from 'node:path';
 
 import { Memento } from '../memento';
 import { GlobalStorage } from '../storage';
-import { TestStep, TestRecord } from '../types';
+import { TestRecord } from '../types';
 import * as toast from '../toast';
 import { errMsg } from '../error';
 import { executeShowTestGenerationPanelCommand } from '../commands';
 
-function isTestRecord(maybe: TestStep | TestRecord): maybe is TestRecord {
-  return 'app_name' in maybe;
-}
-
-export class HistoryProvider
-  implements vscode.TreeDataProvider<TestStep | TestRecord>
-{
+export class HistoryProvider implements vscode.TreeDataProvider<TestRecord> {
   public static readonly viewType = 'scriptiq-history';
 
   constructor(
@@ -23,55 +16,31 @@ export class HistoryProvider
   ) {}
 
   getTreeItem(
-    element: TestStep | TestRecord,
+    element: TestRecord,
   ): vscode.TreeItem | Thenable<vscode.TreeItem> {
-    if (isTestRecord(element)) {
-      return new TestRecordItem(element);
-    }
-    return new TestStepItem(element);
+    return new TestRecordItem(element);
   }
 
   getChildren(
-    element?: TestStep | TestRecord | undefined,
-  ): vscode.ProviderResult<TestRecord[] | TestStep[]> {
-    if (element) {
-      if (isTestRecord(element)) {
-        return element.all_steps;
-      }
-    } else {
-      const ids = this._memento.getTestIDs();
-      const testRecords = this._storage.getTestRecords(ids);
-
-      return testRecords;
-    }
-  }
-
-  getParent(
-    element: TestStep | TestRecord,
-  ): vscode.ProviderResult<TestStep | TestRecord> {
-    if (isTestRecord(element)) {
-      return null;
-    }
+    _element?: TestRecord | undefined,
+  ): vscode.ProviderResult<TestRecord[]> {
     const ids = this._memento.getTestIDs();
     const testRecords = this._storage.getTestRecords(ids);
 
-    // NOTE: A TestStep has no explicit reference to its TestRecord
-    // so we're finding its parent using TestStep's img_url. img_url
-    // contains the jobID so it should be unique enough to find the
-    // parent TestRecord
-    const parent = testRecords.find((tr) => {
-      tr.all_steps?.some((step) => step.img_url === element.img_url);
-    });
-    return parent;
+    return testRecords;
+  }
+
+  getParent(_element: TestRecord): vscode.ProviderResult<TestRecord> {
+    return null;
   }
 
   private _onDidChangeTreeData: vscode.EventEmitter<
-    void | TestStep | TestRecord | (TestStep | TestRecord)[] | null | undefined
+    void | TestRecord | TestRecord[] | null | undefined
   > = new vscode.EventEmitter<
-    void | TestStep | TestRecord | (TestStep | TestRecord)[] | null | undefined
+    void | TestRecord | TestRecord[] | null | undefined
   >();
   readonly onDidChangeTreeData: vscode.Event<
-    void | TestStep | TestRecord | (TestStep | TestRecord)[] | null | undefined
+    void | TestRecord | TestRecord[] | null | undefined
   > = this._onDidChangeTreeData.event;
 
   refresh(): void {
@@ -117,7 +86,7 @@ export class HistoryProvider
 
 class TestRecordItem extends vscode.TreeItem {
   constructor(record: TestRecord) {
-    super(record.app_name, vscode.TreeItemCollapsibleState.Collapsed);
+    super(record.app_name, vscode.TreeItemCollapsibleState.None);
 
     this.contextValue = 'testRecord';
     this.command = {
@@ -126,19 +95,4 @@ class TestRecordItem extends vscode.TreeItem {
       title: 'Show Test',
     };
   }
-}
-
-class TestStepItem extends vscode.TreeItem {
-  constructor(step: TestStep) {
-    super(step.event_reason, vscode.TreeItemCollapsibleState.None);
-  }
-
-  iconPath = path.join(
-    __filename,
-    '..',
-    '..',
-    'media',
-    'icons',
-    'icn-edit-file-outline.svg',
-  );
 }
